26f4d46a1a8ebe8257d3ce7bb00b64ab3880a849
andremoreirasmo <andreluismoreirasmo@gmail.com>
2023-05-25 23:56:01 -0300

    Removed unesed code

--- lib/pages/history_page/history_controller_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:ml_invest_app/pages/history_page/history_controller.dart';
-
-class HistoryControllerBinding implements Bindings {
-  @override
-  void dependencies() {
-    Get.put<HistoryController>(HistoryController(), permanent: true);
-  }
-}
