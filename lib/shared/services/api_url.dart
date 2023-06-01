b51d3936055bee652d0223b5e8706e15a8d903b0
andremoreirasmo <andreluismoreirasmo@gmail.com>
2023-06-01 16:45:42 -0300

    Fixed to use production URL

--- lib/shared/services/api_url.dart
+++ lib/shared/services/api_url.dart
@@ -1,4 +1,4 @@
 class ApiUrl {
-  static const url = 'http://192.168.2.116:3000';
-  // static const url = 'https://ml-invest-api.onrender.com';
+  // static const url = 'http://192.168.2.116:3000';
+  static const url = 'https://ml-invest-api.onrender.com';
 }
