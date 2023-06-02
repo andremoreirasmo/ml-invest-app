abacdf4892a91774f5640001f1c3f6b9828129d7
andremoreirasmo <andreluismoreirasmo@gmail.com>
2023-06-02 12:55:52 -0300

    Show snackbar when server it's starting

--- lib/shared/controllers/Init_controller.dart
+++ lib/shared/controllers/Init_controller.dart
@@ -3,6 +3,7 @@ import 'package:get_storage/get_storage.dart';
 import 'package:ml_invest_app/pages/home/home_controller.dart';
 import 'package:ml_invest_app/shared/controllers/login_controller.dart';
 import 'package:ml_invest_app/shared/utils/routes.dart';
+import 'package:ml_invest_app/shared/utils/snackbar_util.dart';
 
 class InitController extends GetxController {
   @override
@@ -21,8 +22,14 @@ class InitController extends GetxController {
     futures.add(homeController.fetchData());
     futures.add(loginController.loadUser());
 
+    SnackbarUtil.showSnackbar(
+        SnackbarEnum.info,
+        "Nosso servidor está iniciando...",
+        "A primeira requisição pode levar um tempo, pedimos um pouco de paciência.",
+        duration: const Duration(minutes: 1));
     // Wait for all the futures to complete
     await Future.wait(futures);
+    Get.closeCurrentSnackbar();
 
     Get.offNamed(Routes.home);
   }
--- lib/shared/utils/snackbar_util.dart
+++ lib/shared/utils/snackbar_util.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-enum SnackbarEnum { alert, error }
+enum SnackbarEnum { alert, error, info }
 
 class SnackbarUtil {
-  static void showSnackbar(SnackbarEnum type, String title, String message) {
+  static void showSnackbar(SnackbarEnum type, String title, String message,
+      {Duration? duration}) {
     var backgroundColor = const Color.fromARGB(255, 136, 103, 28);
     var icon = Icons.warning_amber;
 
     if (type == SnackbarEnum.error) {
       backgroundColor = Colors.red;
       icon = Icons.error;
+    } else if (type == SnackbarEnum.info) {
+      backgroundColor = Colors.blue;
+      icon = Icons.info;
     }
 
     Get.snackbar(title, message,
@@ -21,6 +25,7 @@ class SnackbarUtil {
           color: Colors.white,
         ),
         snackPosition: SnackPosition.BOTTOM,
-        margin: const EdgeInsets.all(20));
+        margin: const EdgeInsets.all(20),
+        duration: duration);
   }
 }
